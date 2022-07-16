46d417ed3a207961573814e7310e2797fef20435
ail122 <107312683+ail122@users.noreply.github.com>
2022-07-16 23:04:07 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -36,7 +36,7 @@ module.exports = {
         global.colong1 = 'Ciss 📸'
         global.colong2 = '𝚁𝙲𝚃𝙸 𝙱𝙾𝚃'
         global.kontak2 = [
-        ['6289516947204', '𝚒𝚢𝚊𝚗', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://saweria.co/IyanMd', true],
+        ['6289516947204', '𝚒𝚢𝚊𝚗-gg', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://saweria.co/IyanMd', true],
         ['6285770199435', await this.getName('6289516947204@s.whatsapp.net'), ' 𝙣𝙤𝙢𝙤𝙧 𝙗𝙤𝙩', 'https://saweria.co/IyanMd', true], 
         ]
         global.bg = await (await fetch(img)).buffer()
