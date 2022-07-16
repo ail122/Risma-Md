fb7bcd3b93be604afc21dfa34c82bb03631615c2
ail122 <107312683+ail122@users.noreply.github.com>
2022-07-16 22:43:55 +0700

    Update infoalpi.js

--- plugins/infoalpi.js
+++ plugins/infoalpi.js
@@ -1,13 +1,13 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI Alpi* ´ˎ˗
-│ ✎ _Nama_ : Alpi
-│ ✎ _Sekolah_ : SMA 1 BANTARBOLANG
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Jatim
-│ ✎ _Status_ : Pelajar SMA
+│ ✎ _Nama_ : iyan
+│ ✎ _Sekolah_ : lulus
+│ ✎ _Umur_ : 18
+│ ✎ _Asal_ : Jabar
+│ ✎ _Status_ : gkad
 │ ✎ _WhatsApp_ :
-│    wa.me/6285713137903
+│    wa.me/6289516947204
 ╰──────────────
 `.trim(), m)
 }
